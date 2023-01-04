853c1832e8224d3709fb0edf6cb7d5d6966b729f
Nicolas <cuellern@gmail.com>
2023-01-04 21:46:12 +0900

    i learned objects

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+let employee = {}
+let newObject = {}
+
+function updateEmployeeWithKeyAndValue(object, key, value){
+return {...object, [key] : value}
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(object, key, value){
+object[key] =value
+return object
+}
+
+function deleteFromEmployeeByKey(object, key){
+    newObject = {...object}
+    delete newObject[key]
+    return newObject
+return newObject
+}
+
+function destructivelyDeleteFromEmployeeByKey(object, key){
+delete object[key]
+return object
+}
